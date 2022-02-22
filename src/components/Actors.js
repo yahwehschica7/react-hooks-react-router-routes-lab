dd763b1c114ab147eb0ecbf8c1994cb333cf2be0
Jessica Nelson <yahwehschica@yahoo.com>
2022-02-22 11:15:06 -0500

    commit tree d1d5a619243e9fc4627a2cedf6ffedaf913cd29b

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,24 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const actorData = actors.map((actor) => (
+    <div key={actor.name}>
+      <h2>{actor.name}</h2>
+      <ul>
+        {actor.movies.map((movie) => (
+          <li key={movie}>
+            {movie}
+          </li>
+        ))}
+      </ul>
+    </div>
+  ))
+
+  return <div>
+    <h1>Actors Page</h1>
+    {actorData}
+    </div>;
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,20 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const directorsData = directors.map((director) => (
+    <div key={director.name}>
+      <h2>{director.name}</h2>
+        <ul>{director.movies.map((movie)=> (
+          <li key={movie}>{movie}</li>
+        ))}</ul>
+    </div>
+  ))
+
+  return <div>
+    <h1>Directors Page</h1>
+      {directorsData}
+  </div>;
 }
 
 export default Directors;
